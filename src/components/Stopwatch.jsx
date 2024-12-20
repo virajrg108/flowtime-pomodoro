fb7e0cecf0c5abbbcd8f76dfdfa2efe5e66ae534
virajrg108 <viraj.gawde123@gmail.com>
2024-12-20 12:06:26 +0530

    fixed stopwatch issue

--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -2,70 +2,52 @@ import React, { useState, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { Center, Box, Flex, Span, Icon, Button, Input } from '@chakra-ui/react';
 import { FaCirclePlay, FaCirclePause, FaArrowRotateLeft } from "react-icons/fa6";
+import { useStopwatch } from 'react-timer-hook';
 
-
-//FIXME: Stopwatch only starts when the tab is open 
 const Stopwatch = (props) => {
-  const [isActive, setIsActive] = useState(false);
-  const [isPaused, setIsPaused] = useState(true);
-  const [time, setTime] = useState(0);
+  const [activeSession, setActiveSession] = useState(false);
   const [sessionType, setSessionType] = useState('focus');
-  const inputRef = useRef();
-  const minRef = useRef();
-  const secRef = useRef();
+  const { totalSeconds, seconds, minutes, hours, isRunning, start, pause, reset } = useStopwatch({ autoStart: false });
 
-  React.useEffect(() => {
-    let interval = null;
-
-    if (isActive && isPaused === false) {
-      interval = setInterval(() => {
-        setTime((time) => time + 10);
-      }, 10);
-    } else {
-      clearInterval(interval);
-    }
-    return () => {
-      clearInterval(interval);
-    };
-  }, [isActive, isPaused]);
-
-  const handleStart = (breakType) => {
-    setSessionType(breakType);
-    setIsActive(true);
-    setIsPaused(false);
+  const handleStart = (sesType) => {
+    setSessionType(sesType);
+    setActiveSession(true);
+    start();
   };
 
-  const handlePauseResume = () => {
-    setIsPaused(!isPaused);
+  const handlePause = () => {
+    pause();
   };
 
+  const handleResume = () => {
+    start();
+  };
+
+  /* To auto select content of input tag when in focus */
   const handleFocus = (event) => {
     event.target.select();
   }
 
   const handleReset = (...args) => {
-    let d = new Date();
+    reset(null, false); // first argument is offsetTimestamp, second arguement is boolean flag whether the stopwatch should auto start after reset
+    setActiveSession(false);
+
+    const d = new Date();
     if (args.length > 0) {
       console.log("record session");
       console.log("Duration: ",);
-      console.log("Activity Name: ", inputRef.current.value);
       console.log("Start Time: ", new Date());
-      console.log("Duration: ", secRef.current.textContent);
+      console.log("Duration: ", (hours * 60) + (minutes * 60) + seconds);
       props.addNewSession({
-        min: minRef.current.textContent.slice(0, -1),
-        sec: secRef.current.textContent,
-        activity:inputRef.current.value,
-        date: d.getDate()+'-'+d.getMonth()+'-'+d.getFullYear(),
+        duration: (hours * 60) + (minutes * 60) + seconds,
+        date: d.getDate() + '-' + d.getMonth() + '-' + d.getFullYear(),
+        sessionType,
       });
     }
-    setIsActive(false);
-    setTime(0);
-    console.log(inputRef.current.value);
   };
 
-  const StartButton = (
+  const SessionStartState = (
     <div>
-      {/* <Box fontSize="32px">&nbsp;</Box> */}
       <Button onClick={() => handleStart('focus')} marginRight="10px" bg="cyan.700">
         Start Focus Session
       </Button>
@@ -77,7 +59,7 @@ const Stopwatch = (props) => {
       </Flex>
     </div>
   );
-  const ActiveButtons = (
+  const SessionOngoingState = (
     <div>
       {sessionType == 'focus' ?
         <Button
@@ -90,7 +72,7 @@ const Stopwatch = (props) => {
         </Button>}
 
       <Flex justifyContent="center" marginTop="20px">
-        <Icon onClick={handlePauseResume} fontSize="40px" marginRight="20px">{isPaused ? <FaCirclePlay /> : <FaCirclePause />}</Icon>
+        <Icon fontSize="40px" marginRight="20px">{isRunning ? <FaCirclePause onClick={handlePause} /> : <FaCirclePlay onClick={handleResume} />}</Icon>
         <Icon onClick={handleReset} fontSize="40px"><FaArrowRotateLeft /></Icon>
       </Flex>
     </div>
@@ -98,25 +80,18 @@ const Stopwatch = (props) => {
 
   return (
     <Flex gap="1" justify="center" height="100%" alignItems="center" direction="column">
-      <div className="timer">
+      <div>
         <div>
-          {/* {time}<br/> */}
-          <Span textStyle="6xl" fontFamily='"M PLUS Rounded 1c", sans-serif' fontWeight="900" ref={minRef}>
-            {( Math.floor((time / 60000)))}:
-          </Span>
-          <Span textStyle="6xl" fontFamily='"M PLUS Rounded 1c", sans-serif' fontWeight="900" ref={secRef }>
-            {("0" + Math.floor((time / 1000) % 60)).slice(-2)}
-          </Span>
-          <Span textStyle="2xl" marginLeft="5px">
-            {("0" + ((time / 10) % 100)).slice(-2)}
-          </Span>
+          {hours !== 0 ? <Span textStyle="6xl" fontFamily='"M PLUS Rounded 1c", sans-serif' fontWeight="900">{hours < 10 ? "0" + hours : hours}:</Span> : <span></span>}
+          <Span textStyle="6xl" fontFamily='"M PLUS Rounded 1c", sans-serif' fontWeight="900">{minutes < 10 ? "0" + minutes : minutes}:</Span>
+          <Span textStyle="6xl" fontFamily='"M PLUS Rounded 1c", sans-serif' fontWeight="900">{seconds < 10 ? "0" + seconds : seconds}</Span>
         </div>
       </div>
-      <Box marginBottom="10px">
-        <Input placeholder="Activity Name" ref={inputRef} defaultValue="FocusSession" onFocus={handleFocus} />
-      </Box>
+      {/* <Box marginBottom="10px">
+        <Input placeholder="Activity Name" ref={inputRef} defaultValue={sessionType === "focus" ? "FocusSession" : "BreakSession"} onFocus={handleFocus} />
+      </Box> */}
       <Box>
-        <div>{isActive ? ActiveButtons : StartButton}</div>
+        <div>{activeSession ? SessionOngoingState : SessionStartState}</div>
       </Box>
     </Flex>
   );
